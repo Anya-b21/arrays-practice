df0831a21934cfddbfd5973ad640420cd1a4097e
Kurbakov <mail@mail.com>
2026-09-16 14:00:00 +0300

    Task1: статистика случайного массива

--- Task1.Statistics/Program.cs
+++ Task1.Statistics/Program.cs
@@ -1,1 +1,43 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Task1.Statistics
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Создаём массив из 10 случайных чисел [1, 100]
+            Random rnd = new Random();
+            int[] arr = new int[10];
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = rnd.Next(1, 101);
+
+            Console.WriteLine("Массив: " + string.Join(", ", arr));
+
+            // Сумма
+            long sum = 0;
+            for (int i = 0; i < arr.Length; i++) sum += arr[i];
+            Console.WriteLine("Сумма: " + sum);
+
+            // Произведение
+            double product = 1;
+            for (int i = 0; i < arr.Length; i++) product *= arr[i];
+            Console.WriteLine("Произведение: " + product.ToString("E2"));
+
+            // Количество чётных
+            int evenCount = 0;
+            for (int i = 0; i < arr.Length; i++)
+                if (arr[i] % 2 == 0) evenCount++;
+            Console.WriteLine("Чётных чисел: " + evenCount);
+
+            // Среднее арифметическое
+            double avg = (double)sum / arr.Length;
+
+            // Количество элементов больше среднего
+            int aboveAvg = 0;
+            for (int i = 0; i < arr.Length; i++)
+                if (arr[i] > avg) aboveAvg++;
+            Console.WriteLine($"Больше среднего ({avg:F1}): {aboveAvg}");
+        }
+    }
+}
