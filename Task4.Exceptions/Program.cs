38b3aa218be8d33ce06d0893a8f4c645183098d5
Kurbakov <mail@mail.com>
2026-09-17 14:55:38 +0300

    Task4: обработка исключений при вводе и выводе

--- Task4.Exceptions/Program.cs
+++ Task4.Exceptions/Program.cs
@@ -1,1 +1,52 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Task4.Exceptions
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] arr = new int[5];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                while (true)
+                {
+                    Console.Write($"Введите элемент [{i}]: ");
+                    try
+                    {
+                        arr[i] = int.Parse(Console.ReadLine());
+                        break;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Ошибка: Введите целое число!");
+                    }
+                    catch (OverflowException)
+                    {
+                        Console.WriteLine("Ошибка: Число слишком большое!");
+                    }
+                }
+            }
+
+            while (true)
+            {
+                Console.Write("Введите индекс для вывода (0-4): ");
+                try
+                {
+                    int index = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"Элемент [{index}] = {arr[index]}");
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Ошибка: Введите целое число!");
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Ошибка: Индекс вне границ массива.");
+                }
+            }
+        }
+    }
+}
